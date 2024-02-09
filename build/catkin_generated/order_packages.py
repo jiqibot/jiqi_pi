3658d53eecc4d2ae76965f81bac3687d93df8149
pi <jiqibot32@gmail.com>
2024-02-09 13:52:08 +0000

    Adding Map and Jiqi Nav

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jiqi/jiqi_pi/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/jiqi/jiqi_pi/devel;/opt/ros/noetic'.split(';') if '/home/jiqi/jiqi_pi/devel;/opt/ros/noetic' != '' else []
---
+++ build/gps_waypointer/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jiqi/jiqi_pi/src/gps_waypointer
+# Build directory: /home/jiqi/jiqi_pi/build/gps_waypointer
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
