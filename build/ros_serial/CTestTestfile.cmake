d3114699375d298b1da655e37a60d6f91efd1178
Jiqi <151838176+jiqibot@users.noreply.github.com>
2024-02-16 10:53:57 +0000

    Delete build directory

--- build/ros_serial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jiqi/jiqi_pi/src/ros_serial
-# Build directory: /home/jiqi/jiqi_pi/build/ros_serial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
