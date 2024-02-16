8afcd79af04b58c28617539dad78043d72c57c02
pi <jiqibot32@gmail.com>
2024-02-16 12:30:56 +0000

    Removing bad files.

--- build/articubot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jiqi/jiqi_pi/src/articubot
-# Build directory: /home/jiqi/jiqi_pi/build/articubot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/gps_waypointer/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jiqi/jiqi_pi/src/gps_waypointer
-# Build directory: /home/jiqi/jiqi_pi/build/gps_waypointer
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
