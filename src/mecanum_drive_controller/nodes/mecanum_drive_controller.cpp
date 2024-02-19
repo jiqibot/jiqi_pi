97f09c428ba8acb86d2abc67a06b33e6e7123506
Tom <jiqibot32@gmail.com>
2024-02-19 14:18:59 +0000

    Corrected cmd_vel callback function

--- src/mecanum_drive_controller/nodes/mecanum_drive_controller.cpp
+++ src/mecanum_drive_controller/nodes/mecanum_drive_controller.cpp
@@ -62,11 +62,11 @@ class MDCNode
             }
         }
 
-        void twistCallback(const geometry_msgs::Twist::ConstPtr& twist)
+        void twistCallback(const geometry_msgs::Twist::ConstPtr& cmd_vel)
         {
-            linear_x_velocity = twist->linear.x;
-            linear_y_velocity = twist->linear.y;
-            angular_velocity = twist->angular.z;
+            linear_x_velocity = cmd_vel->linear.x;
+            linear_y_velocity = cmd_vel->linear.y;
+            angular_velocity = cmd_vel->angular.z;
             last_twist_time = ros::Time::now();
         }
 
