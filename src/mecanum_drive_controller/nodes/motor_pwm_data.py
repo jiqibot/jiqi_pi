32c09238e8e851ddb6c1666d1fb29213b7ec7d1d
Tom <jiqibot32@gmail.com>
2024-02-19 20:56:19 +0000

    Edited listener node

--- src/mecanum_drive_controller/nodes/motor_pwm_data.py
+++ src/mecanum_drive_controller/nodes/motor_pwm_data.py
@@ -2,12 +2,11 @@
 
 import rospy
 # This library is used as we are receiving Int32 messages - used as velocities are in the range 0-255
-from std_msgs.msg import Int16MultiArray
+from std_msgs.msg import Int16
 
 # Callback functions are called everytime a message is received - prints wheel speeds to screen
-def interpret(wheel_speed):
-    print("FL: ", wheel_speed.data[0], "   FR: ", wheel_speed.data[1])
-    print("RL: ", wheel_speed.data[2], "   RR: ", wheel_speed.data[3])
+def callback_fl(motor_pwm):
+    print("FL: %s" %motor_pwm.data)
 
 # Initialise subscriber node - setting 'anonymous=True' to ensure node has unique name - ensured via adding random
     # numbers to end of node name
@@ -15,7 +14,7 @@ rospy.init_node('interpret_test', anonymous=True)
 
 # Subscribe to the appropriate topics - names must match with Arduino code, specify type of message to be received and
     # callback function to be used
-rospy.Subscriber("motor_pwm_data", Int16MultiArray, interpret)
+rospy.Subscriber("fl_motor_pwm_data", Int16, callback_fl)
 
 # Spin the code
 rospy.spin()
