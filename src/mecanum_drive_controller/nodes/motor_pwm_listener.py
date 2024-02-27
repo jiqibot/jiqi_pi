f9493e33014ffe7db40a1dd1edf93c58fa483f6e
Tom <jiqibot32@gmail.com>
2024-02-27 23:43:13 +0000

    Added custom message files (adjusted CMakeLists.txt and package.xml accordingly) Added launch file for laptop testing

--- src/mecanum_drive_controller/nodes/motor_pwm_listener.py
+++ src/mecanum_drive_controller/nodes/motor_pwm_listener.py
@@ -2,14 +2,14 @@
 
 import rospy
 # This library is used as we are receiving Float32MultiArray messages - used as velocities are in the range 0-255
-from std_msgs.msg import Float32MultiArray
+from mecanum_drive_controller.msg import pwm_data
 
 # Callback functions are called everytime a message is received - prints wheel speeds to screen
 def callback_pwm(motor_pwm):
-    print("FL: %s" %motor_pwm.data[0], end="")
-    print(", FR: %s" %motor_pwm.data[1], end="")
-    print(", RL: %s" %motor_pwm.data[2], end="")
-    print(", RR: %s" %motor_pwm.data[3])
+    print("FL: %s" %motor_pwm.fl, end="")
+    print(", FR: %s" %motor_pwm.fr, end="")
+    print(", RL: %s" %motor_pwm.rl, end="")
+    print(", RR: %s" %motor_pwm.rr)
 
 # Initialise subscriber node - setting 'anonymous=True' to ensure node has unique name - ensured via adding random
     # numbers to end of node name
@@ -17,7 +17,7 @@ rospy.init_node('motor_pwm_listener', anonymous=True)
 
 # Subscribe to the appropriate topics - names must match with Arduino code, specify type of message to be received and
     # callback function to be used
-rospy.Subscriber("motor_pwm_data", Float32MultiArray, callback_pwm)
+rospy.Subscriber("motor_pwm_data", pwm_data, callback_pwm)
 
 # Spin the code
 rospy.spin()
