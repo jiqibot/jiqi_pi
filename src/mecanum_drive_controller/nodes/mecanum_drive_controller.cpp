b592df7683d75b8290db17c085062c3dd36e4052
kai <jiqibot32@gmail.com>
2024-03-01 15:06:11 +0000

    added stop/slow function

--- src/mecanum_drive_controller/nodes/mecanum_drive_controller.cpp
+++ src/mecanum_drive_controller/nodes/mecanum_drive_controller.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include <std_msgs/Bool.h>
 
 #include "../include/mdc_controller.hpp"
 #include "mecanum_drive_controller/motor_data.h"
@@ -20,6 +21,9 @@ class MDCNode
             ros::NodeHandle nh;
             motor_pps_pub = nh.advertise<mecanum_drive_controller::motor_data>("motor_pps_data", 1);
             twist_sub = nh.subscribe<geometry_msgs::Twist>("cmd_vel", 1, &MDCNode::twistCallback, this);
+            
+            stop_obj_sub = nh.subscribe<std_msgs::Bool>("front_object_close_stop", 1, &MDCNode::stopCallback, this);
+            slow_obj_sub = nh.subscribe<std_msgs::Bool>("front_object_close_slow", 1, &MDCNode::slowCallback, this);
 
             nh.param("pulses_per_meter", pulses_per_meter, 536);
             nh.param("wheel_separation_width", wheel_separation_width, 0.58);
@@ -34,44 +38,52 @@ class MDCNode
             controller.setMaxMotorSpeed(max_motor_speed);
 
             ros::Rate loop_rate(rate);
-            last_twist_time = ros::Time::now();
 
             while (ros::ok())
             {
-                publish();
+                personCheck();
                 ros::spinOnce();
                 loop_rate.sleep();
             }
-
-            shutdown();
         }
 
-        void shutdown()
-        {
-            mecanum_drive_controller::motor_data stop_msg;
-            stop_msg.fl = 0;
-            stop_msg.fr = 0;
-            stop_msg.rl = 0;
-            stop_msg.rr = 0;
-            motor_pps_pub.publish(stop_msg);
-        }
-
+        
     private:
         // METHODS
+
+        void personCheck()
+        {
+            if (stop_msg = true)
+            {
+                mecanum_drive_controller::motor_data stop_msg;
+                stop_msg.fl = 0;
+                stop_msg.fr = 0;
+                stop_msg.rl = 0;
+                stop_msg.rr = 0;
+                motor_pps_pub.publish(stop_msg); 
+            }
+
+            else if (slow_msg = true)
+            {
+                max_motor_speed = 128;
+            }
+
+            else
+            {publish();}
+
+            
+        }
+
         void publish()
         {
-            if ((ros::Time::now() - last_twist_time).toSec() < timeout) {
-                auto speeds = controller.motorSpeed(linear_x_velocity, linear_y_velocity, angular_velocity);
+            auto speeds = controller.motorSpeed(linear_x_velocity, linear_y_velocity, angular_velocity);
 
-                motor_pps.fl = speeds.FL;
-                motor_pps.fr = speeds.FR;
-                motor_pps.rl = speeds.RL;
-                motor_pps.rr = speeds.RR;
+            motor_pps.fl = speeds.FL;
+            motor_pps.fr = speeds.FR;
+            motor_pps.rl = speeds.RL;
+            motor_pps.rr = speeds.RR;
 
-                motor_pps_pub.publish(motor_pps);
-            } else {
-                shutdown();
-            }
+            motor_pps_pub.publish(motor_pps);
         }
 
         void twistCallback(const geometry_msgs::Twist::ConstPtr& cmd_vel)
@@ -79,7 +91,16 @@ class MDCNode
             linear_x_velocity = cmd_vel->linear.x;
             linear_y_velocity = cmd_vel->linear.y;
             angular_velocity = cmd_vel->angular.z;
-            last_twist_time = ros::Time::now();
+        }
+
+        void stopCallback(const std_msgs::Bool::ConstPtr& msg)
+        {
+            stop_msg = msg->data;
+        }
+
+        void slowCallback(const std_msgs::Bool::ConstPtr& msg)
+        {
+            slow_msg = msg->data;
         }
 
         // MEMBER VARIABLES
@@ -88,12 +109,14 @@ class MDCNode
         double wheel_separation_width, wheel_separation_length;
         int max_motor_speed, pulses_per_meter;
         double rate, timeout;
+        bool stop_msg, slow_msg;
         mecanum_drive_controller::motor_data motor_pps;
 
         // ALL ROS STUFF: SUBS, PUBS AND SERVICES
-        ros::Time last_twist_time;
         ros::Publisher motor_pps_pub;
         ros::Subscriber twist_sub;
+        ros::Subscriber slow_obj_sub;
+        ros::Subscriber stop_obj_sub;
 };
 
 int main(int argc, char **argv)
