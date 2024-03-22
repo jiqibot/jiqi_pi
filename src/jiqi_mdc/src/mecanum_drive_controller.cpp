deee31832ef79c73e2b4913fd475097495eb9b02
Tom <jiqibot32@gmail.com>
2024-03-22 15:02:02 +0000

    Temp change

--- src/jiqi_mdc/src/mecanum_drive_controller.cpp
+++ src/jiqi_mdc/src/mecanum_drive_controller.cpp
@@ -3,17 +3,19 @@
 #include <std_msgs/Bool.h>
 
 #include "../include/mdc_controller.hpp"
-#include "jiqi_mdc/motor_data.h"
+// #include "jiqi_mdc/motor_data.h"
+#include <std_msgs/Float32.h>
 
 class MDCNode
 {
     public:
         MDCNode() : controller(), linear_x_velocity(0.0), linear_y_velocity(0.0), angular_velocity(0.0)
         {
-            motor_pps.fl = 0;
-            motor_pps.fr = 0;
-            motor_pps.rl = 0;
-            motor_pps.rr = 0;
+            motor_pps.data = 0;
+            // motor_pps.fl = 0;
+            // motor_pps.fr = 0;
+            // motor_pps.rl = 0;
+            // motor_pps.rr = 0;
             stop_msg = false;
             slow_msg = false;
         }
@@ -21,7 +23,8 @@ class MDCNode
         void main()
         {
             ros::NodeHandle nh;
-            motor_pps_pub = nh.advertise<jiqi_mdc::motor_data>("motor_pps_data", 1);
+            // motor_pps_pub = nh.advertise<jiqi_mdc::motor_data>("motor_pps_data", 1);
+            motor_pps_pub = nh.advertise<std_msgs::Float32>("motor_pps_data", 1);
             twist_sub = nh.subscribe<geometry_msgs::Twist>("cmd_vel", 1, &MDCNode::twistCallback, this);
             
             stop_obj_sub = nh.subscribe<std_msgs::Bool>("front_object_close_stop", 1, &MDCNode::stopCallback, this);
@@ -57,10 +60,11 @@ class MDCNode
         {
             if (stop_msg)
             {
-                motor_pps.fl = 0;
-                motor_pps.fr = 0;
-                motor_pps.rl = 0;
-                motor_pps.rr = 0;
+                motor_pps.data = 0;
+                // motor_pps.fl = 0;
+                // motor_pps.fr = 0;
+                // motor_pps.rl = 0;
+                // motor_pps.rr = 0;
                 motor_pps_pub.publish(motor_pps);
 		std::cout << "Before ROS_INFO" << std::endl;
                 ROS_INFO_STREAM("Stopped");
@@ -87,10 +91,11 @@ class MDCNode
         {
             auto speeds = controller.motorSpeed(linear_x_velocity, linear_y_velocity, angular_velocity);
 
-            motor_pps.fl = speeds.FL;
-            motor_pps.fr = speeds.FR;
-            motor_pps.rl = speeds.RL;
-            motor_pps.rr = speeds.RR;
+            motor_pps.data = speeds.FL;
+            // motor_pps.fl = speeds.FL;
+            // motor_pps.fr = speeds.FR;
+            // motor_pps.rl = speeds.RL;
+            // motor_pps.rr = speeds.RR;
 
             motor_pps_pub.publish(motor_pps);
         }
@@ -119,7 +124,8 @@ class MDCNode
         int max_motor_speed, pulses_per_meter;
         double rate, timeout;
         bool stop_msg, slow_msg;
-        jiqi_mdc::motor_data motor_pps;
+        // jiqi_mdc::motor_data motor_pps;
+        std_msgs::Float32 motor_pps;
 
         // ALL ROS STUFF: SUBS, PUBS AND SERVICES
         ros::Publisher motor_pps_pub;
